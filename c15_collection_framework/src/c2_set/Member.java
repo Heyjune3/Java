6d8097192d5f280514839d8c2e42962f978464f0
Heyjune3 <kfd45674@gmail.com>
2023-11-30 14:53:48 +0900

    컬렉션 set

---
+++ c15_collection_framework/src/c2_set/Member.java
@@ -0,0 +1,39 @@
+package c2_set;
+
+public class Member {
+	
+	private String name;
+	private int age;
+	
+	public Member(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	// alt + s + v
+	@Override
+	public int hashCode() {
+		System.out.println("hashCode 호출 : " + this.age);
+		return this.age;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("equals 호출");
+		if(obj instanceof Member) {
+			Member m = (Member)obj;
+			if(m.name != null && m.name.equals(this.name)) {
+				System.out.println("this.name : " + this.name);
+				System.out.println("param name : " + m.name);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
+	}	
+}
