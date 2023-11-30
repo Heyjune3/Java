41e49e535fabff8ea90e8e6eccb7897895a53ee7
Heyjune3 <kfd45674@gmail.com>
2023-11-30 15:07:30 +0900

    Comparator Arrays

--- c15_collection_framework/src/c8_comparator/ComparatorExample.java
+++ c15_collection_framework/src/c8_comparator/ComparatorExample.java
@@ -1,5 +1,7 @@
 package c8_comparator;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class ComparatorExample {
@@ -13,6 +15,41 @@ public class ComparatorExample {
 		treeSet.add(new Fruit("수박",10000));
 		System.out.println(treeSet);
 		
+		Comparator<Fruit> compare = new Comparator<Fruit>() {
+			@Override
+			public int compare(Fruit o1, Fruit o2) {
+				return o1.getName().hashCode() - o2.getName().hashCode();
+			}
+		};
+
+		TreeSet<Fruit> treeSet2 = new TreeSet<>(compare);
+		treeSet2.add(new Fruit("포도",3000));
+		treeSet2.add(new Fruit("딸기",1500));
+		treeSet2.add(new Fruit("수박",10000));
+		System.out.println(treeSet2);
+		
+		Integer[] arrays = new Integer[] {10,70,90,80};
+		Arrays.sort(arrays);
+		System.out.println(Arrays.toString(arrays));
+		
+		Arrays.sort(arrays, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;	 // 오름차순(역순)정렬
+			}
+		});
+		System.out.println(Arrays.toString(arrays));
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
