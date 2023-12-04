7b29c24bb937cb48c196097579b248beb57a9652
Heyjune3 <kfd45674@gmail.com>
2023-12-04 16:29:43 +0900

    File Input,Output Stream

--- c17_IO_stream/src/f03_input/ReadExample2.java
+++ c17_IO_stream/src/f03_input/ReadExample2.java
@@ -1,9 +1,52 @@
 package f03_input;
 
-public class ReadExample2 {
+import java.io.*;
+import java.util.Arrays;
 
-	public static void main(String[] args) {
+public class ReadExample2 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		InputStream is = new FileInputStream("C:\\Temp\\file2.txt");
+		byte[] bytes = new byte[100];
+		int readBytes = is.read(bytes);
+		System.out.println(Arrays.toString(bytes));
+		System.out.println(readBytes);
+		String result = new String(bytes);
+		System.out.println(result);
+		is.close();
+		
+		// 한번 연결이 끊긴 스트림은 재사용 불가
+		is = new FileInputStream("C:\\Temp\\file2.txt");
+		int available = is.available();
+		System.out.println("읽어 들일 수 있는 파일의 크기 : " + available);
+		bytes = new byte[available];
+		
+		readBytes = is.read(bytes, 0, bytes.length);
+		System.out.println(Arrays.toString(bytes));
+		System.out.println(readBytes);
+		available = is.available();
+		System.out.println("읽어 들일 수 있는 파일의 크기 : " + available);
+		is.close();
+		
+		
+		
 		
 	}
-
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
