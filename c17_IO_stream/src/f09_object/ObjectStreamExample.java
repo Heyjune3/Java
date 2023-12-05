07f0a6e57eada6a755934e65a5ef84c97dd7d552
Heyjune3 <kfd45674@gmail.com>
2023-12-05 13:57:22 +0900

    data_print

---
+++ c17_IO_stream/src/f09_object/ObjectStreamExample.java
@@ -0,0 +1,14 @@
+package f09_object;
+
+import java.io.FileOutputStream;
+
+public class ObjectStreamExample {
+
+	public static void main(String[] args) {
+
+		FileOutputStream fos = new FileOutputStream("object.dat");
+				
+		
+	}
+
+}
