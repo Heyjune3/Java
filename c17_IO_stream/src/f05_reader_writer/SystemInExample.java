25dca8259f93ce3f16d871329d79ab251878d0a9
Heyjune3 <kfd45674@gmail.com>
2023-12-05 11:16:35 +0900

    바이트단위 읽기 read로 아스키코드 출력

--- c17_IO_stream/src/f05_reader_writer/SystemInExample.java
+++ c17_IO_stream/src/f05_reader_writer/SystemInExample.java
@@ -1,9 +1,35 @@
 package f05_reader_writer;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class SystemInExample {
 
 	public static void main(String[] args) {
 
+		InputStream is = System.in;
+		
+		try {
+			while(true) {
+				int i = is.read();  // 바이트 단위로 읽기
+				System.out.println(i);
+				if(i == 'q') break;
+				System.out.println("입력받은 값은 : " + (char)i);
+/*
+A
+65
+입력받은 값은 : A
+13
+입력받은 값은 :  // Enter키 
+
+10
+입력받은 값은 : 
+*/
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
